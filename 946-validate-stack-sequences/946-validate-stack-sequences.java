6c509c80f2e4c33b5b8ec982c10cf77e311c258f
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-09-16 15:13:09 +0530

    Update 946-validate-stack-sequences.java

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,3 +1,4 @@
+// Method 01
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> s = new Stack<>();
@@ -10,5 +11,37 @@ class Solution {
             }
         }
         return s.isEmpty();
+    }
+}
+
+
+// Method 02
+class Solution {
+   public static boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> s = new Stack<>();
+        s.push(pushed[0]);
+        int j =0;
+        int i=1;
+        int counter =0;
+        while(j<popped.length && counter!= popped.length  ){
+           
+            if(!s.isEmpty() && popped[j]==s.peek()){
+                j++;
+//                i--;
+                s.pop();
+                continue;
+            }
+                if(i< pushed.length){
+                    s.push(pushed[i]);
+                    i++;    
+                }
+             counter++;
+
+        }
+        if(s.isEmpty()){
+            return true;
+        }
+
+        return false;
     }
 }
