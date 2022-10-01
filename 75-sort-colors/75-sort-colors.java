88c77323d02507ade1a4f276d2a22a1179f5064b
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-10-01 08:38:09 +0530

    Time: 22 ms (5.64%), Space: 42.4 MB (55.48%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-      Arrays.sort(nums);
+        int count[]={0,0,0};
+        for(int i =0;i<nums.length;i++){
+            count[nums[i]] +=1;
+        }
+        for(int i =0;i<count.length;i++){
+            System.out.print("Theee count array "+i+ " count will be ");
+            System.out.print(count[i]+" ");
+            System.out.println();
+        }
+        int j=0;
+        for(int i =0;i<nums.length;i++){
+            if(count[j]==0){
+                ++j;
+                --i;
+            }
+            else{
+                nums[i] = j;
+                System.out.println("The curr index "+ i + " value is " + nums[i]);
+                --count[j];
+                System.out.println("The j is "+ j);
+            }
+        }
     }
 }
