4c770903d301334bcfc816ada9fe9093bf5667af
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-10-10 12:00:15 +0530

    Time: 435 ms (5.09%), Space: 98.3 MB (5.37%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,6 +1,7 @@
 class Solution {
     public int minSwaps(String str) {
         Stack<Character> s =new Stack<>();
+        int mismatch=0;
         for(char ch :str.toCharArray()){
             if(ch=='['){
                 s.push(ch);
@@ -11,20 +12,22 @@ class Solution {
                 }
                 else{
                     s.push(ch);
+                    mismatch++;
                 }
             }
         }
 //         Stack Contains invalid expreession
-        int a =0,b=0;
-        while(!s.isEmpty()){
-            if(s.pop()=='['){
-                b++;
-            }
-            else{
-                a++;
-            }
-        }
-        int ans  = (a+1)/2 + (b+1)/2;
-        return ans/2;
+        // int a =0,b=0;
+        // while(!s.isEmpty()){
+        //     if(s.pop()=='['){
+        //         b++;
+        //     }
+        //     else{
+        //         a++;
+        //     }
+        // }
+        // int ans  = (a+1)/2 + (b+1)/2;
+        // return ans/2;
+        return (mismatch+1)/2;
     }
 }
