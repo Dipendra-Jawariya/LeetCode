ea22e4d20e2f7725b160c789bb20fdd11a2f8a6c
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-11-02 10:50:32 +0530

    Time: 1 ms (98.03%), Space: 43.4 MB (66.07%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,43 +1,25 @@
+// Optimized solution
 class Solution {
-    private boolean matching(char a, char b){
-        return (a=='(' && b==')');
-    }
-    private boolean valid(char[] curr){
-        Stack<Character> s = new Stack<>();
-        for(int i=0;i<curr.length;i++){
-            if(curr[i]=='('){
-                s.push(curr[i]);
-            }
-            else{
-                if(s.isEmpty() || s.peek()!='('){
-                    return false;
-                }
-                else if(matching(s.peek(),curr[i])){
-                    s.pop();
-                }
-            }
-        }
-        return s.isEmpty();
-    }
-    private void generateAll(int ind,List<String> ans,char[] curr){
+    private void generateAll(int ind,List<String> ans,char[] curr,int open,int close,int max){
         if(ind==curr.length){
-            for(char ch:curr){
-                System.out.print(ch);
-            }
-                System.out.println();
-            if(valid(curr)){
+            if(open==close ){
                 ans.add(new String(curr));
             }
              return;
         }
-        curr[ind] = '(';
-        generateAll(ind+1,ans,curr);
-        curr[ind] = ')';
-        generateAll(ind+1,ans,curr);
+        if(open<max){
+            curr[ind] = '(';
+        generateAll(ind+1,ans,curr,open+1,close,max);
+        }
+        if(close < open){
+            curr[ind] = ')';
+            generateAll(ind+1,ans,curr,open,close+1,max);
+        }
+        
     }
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<String>();
-        generateAll(0,ans,new char[2 * n]);
+        generateAll(0,ans,new char[2 * n],0,0,n);
         return ans;
     }
 }
