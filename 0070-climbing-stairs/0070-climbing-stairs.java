691b70befbaded1891e6e93029b1af994c1441dd
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-12-05 11:18:08 +0530

    Time: 0 ms (100.00%), Space: 41 MB (37.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,24 @@
 class Solution {
     public int climbStairs(int n) {
-//         Tabulation
-         int dp[] = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i =2; i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+//         Space Optimization
+        int prev2 = 1;
+        int prev = 1;
+        for(int i=2;i<=n;i++){
+            int curri = prev+ prev2;
+             prev2 = prev;
+            prev = curri;
         }
-        return dp[n];
+        return prev;
+        
+//         Tabulation
+        //  int dp[] = new int[n+1];
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i =2; i<=n;i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+        
 //         Memoization
         // int dp[] = new int[n+1];
         // Arrays.fill(dp, -1);
