b155dba9bd5a4e782e20449c4aa4063fb138527c
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-12-14 19:49:19 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (84.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -19,16 +19,32 @@ class Solution {
         Arrays.fill(dp,-1);
         // return solve(n-1,nums,dp);
 //         Tabulation
-        dp[0] = arr[0];
-        int max = Integer.MIN_VALUE;
+        // dp[0] = arr[0];
+        // int max = Integer.MIN_VALUE;
+        // for(int i =1;i<n;i++){
+        //     int pick = arr[i];
+        //     if(i >1){
+        //         pick+= dp[i-2];
+        //     }
+        //     int skip = dp[i-1];
+        //     dp[i] = Math.max(pick,skip);
+        // }
+        // return dp[n-1];
+        
+        
+//         Space Optimization
+        int prev2 = 0;
+        int prev = arr[0];
         for(int i =1;i<n;i++){
-            int pick = arr[i];
-            if(i >1){
-                pick+= dp[i-2];
+            int rob= arr[i];
+            if(i>1){
+                rob += prev2;
             }
-            int skip = dp[i-1];
-            dp[i] = Math.max(pick,skip);
-        }
-        return dp[n-1];
+            int skip = 0 + prev;
+            int curr_i = Math.max(rob,skip);
+            prev2 = prev;
+            prev = curr_i;
+         }
+        return prev;
     }
 }
