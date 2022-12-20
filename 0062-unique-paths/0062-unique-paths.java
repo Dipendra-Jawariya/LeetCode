1d037b560196280d4f00987f1f767fd34f208c15
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-12-20 23:03:27 +0530

    Time: 2 ms (30.69%), Space: 41.2 MB (22.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -12,6 +12,26 @@ class Solution {
         for(int row[]:dp){
             Arrays.fill(row,-1);
         }
-       return ways(m-1,n-1,dp);
+       // return ways(m-1,n-1,dp);
+        
+        // Tabulation
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left =0;
+                if(i>0){
+                    up = dp[i-1][j];
+                }
+                if(j>0){
+                    left = dp[i][j-1];
+                }
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
