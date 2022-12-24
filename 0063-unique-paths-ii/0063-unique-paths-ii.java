a1060ea867348deeb6b7cf6a1f93994d5070792f
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-12-24 18:26:11 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (89.77%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -17,6 +17,27 @@ class Solution {
         for(int row[]: dp){
             Arrays.fill(row,-1);
         }
-        return func(n-1,m-1,obstacleGrid,dp);    
+        // return func(n-1,m-1,obstacleGrid,dp);    
+        
+//         Tabulation
+        dp[0][0] = 0;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(obstacleGrid[i][j] == 1) dp[i][j] =0;
+                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                     int up = 0;
+                     int left = 0;
+                     if(i>0){
+                        up = dp[i-1][j];
+                     }
+                        if(j>0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] =  up + left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
