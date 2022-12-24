546f19e5a51a11386a3e077faa9ca38b8b423c1f
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-12-24 19:38:51 +0530

    Time: 2 ms (91.11%), Space: 45 MB (82.43%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -12,35 +12,63 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int dp[][]  = new int[n][m];
-        for(int row[] :dp){
-            Arrays.fill(row,-1);
-        }
+        // int dp[][]  = new int[n][m];
+        // for(int row[] :dp){
+        //     Arrays.fill(row,-1);
+        // }
         // return solve(n - 1,m - 1,grid,dp);
 //         Tabulation
-        for(int i=0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(i==0 && j==0){
-                    dp[i][j] = grid[i][j];
-                }
-                else{
-                    int up =0;
-                    int left = 0;
-                    if(i>0){
-                         up = grid[i][j] + dp[i-1][j];
-                    }else{
-                        up = (int) 1e9;
-                    }
-                    if(j>0){
-                        left = grid[i][j] + dp[i][j-1];
-                    }else{
-                        left = (int) 1e9; 
-                    }
-                    dp[i][j] = Math.min(up,left);
-                }
+//         for(int i=0;i<n;i++){
+//             for(int j =0;j<m;j++){
+//                 if(i==0 && j==0){
+//                     dp[i][j] = grid[i][j];
+//                 }
+//                 else{
+//                     int up =grid[i][j];
+//                     int left = grid[i][j];
+//                     if(i>0){
+                            //REQUIRING PREVIOUS ROW'S J COLUMN
+//                          up += dp[i-1][j];
+//                     }else{
+//                         up += (int) 1e9;
+//                     }
+//                     if(j>0){
+                            //CURRENT ROW  J-1 COLUMN
+//                         left += dp[i][j-1];
+//                     }else{
+//                         left += (int) 1e9; 
+//                     }
+//                     dp[i][j] = Math.min(up,left);
+//                 }
                 
-            }
-        } 
-        return dp[n-1][m-1];
+//             }
+//         } 
+//         return dp[n-1][m-1];
+        
+        
+//         SPACE OPTIMIZATION
+        int prev[] = new int[m];
+        Arrays.fill(prev,0);
+//         i = n
+        for(int i=0;i<n;i++){
+            int curr[] = new int[m];
+            // Arrays.fill()
+             for(int j =0;j<m;j++){
+                 if(i==0 && j==0) curr[j] = grid[i][j];
+                 else{
+                     int up = grid[i][j];
+                     if(i>0) up+= prev[j];
+                     else up += 1e9;
+                     
+                     int left = grid[i][j];
+                     if(j>0) left += curr[j-1];
+                     else left+= 1e9;
+                     
+                     curr[j] = Math.min(up,left);
+                 }
+             }
+            prev = curr;
+        }
+        return prev[m-1];
     }
 }
