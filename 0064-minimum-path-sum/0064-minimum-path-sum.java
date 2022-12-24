cbfa039ace7a08f7687fc643f685d5e9525ffa9e
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-12-24 19:18:32 +0530

    Time: 3 ms (68.14%), Space: 44.9 MB (86.38%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -16,6 +16,31 @@ class Solution {
         for(int row[] :dp){
             Arrays.fill(row,-1);
         }
-        return solve(n - 1,m - 1,grid,dp);
+        // return solve(n - 1,m - 1,grid,dp);
+//         Tabulation
+        for(int i=0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j] = grid[i][j];
+                }
+                else{
+                    int up =0;
+                    int left = 0;
+                    if(i>0){
+                         up = grid[i][j] + dp[i-1][j];
+                    }else{
+                        up = (int) 1e9;
+                    }
+                    if(j>0){
+                        left = grid[i][j] + dp[i][j-1];
+                    }else{
+                        left = (int) 1e9; 
+                    }
+                    dp[i][j] = Math.min(up,left);
+                }
+                
+            }
+        } 
+        return dp[n-1][m-1];
     }
 }
