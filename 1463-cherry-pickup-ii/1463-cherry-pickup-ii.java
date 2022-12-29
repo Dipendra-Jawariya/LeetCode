47f4a68b9794e598058f05d0b33647c27abb48ae
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2022-12-30 00:45:09 +0530

    Time: 67 ms (25.31%), Space: 43.3 MB (71.03%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -22,14 +22,50 @@ class Solution {
         return dp[i][j1][j2] = maxi;
     }
     public int cherryPickup(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
-        int dp[][][] = new int[r][c][c];
-        for(int rows[][]: dp){
-            for(int row[] :rows){
-                Arrays.fill(row, -1);
+        int n = grid.length;
+        int m = grid[0].length;
+        // int dp[][][] = new int[n][m][m];
+        // for(int rows[][]: dp){
+        //     for(int row[] :rows){
+        //         Arrays.fill(row, -1);
+        //     }
+        // }
+        // return recMemo(0,0,c-1,r,c,grid,dp);
+        
+        // TABULATION
+        
+        // base case
+        int dp[][][]= new int[n][m][m];
+        for(int j1 = 0;j1 < m;j1++){
+            for(int j2 = 0; j2 <m; j2++){
+                if(j1 == j2){
+                    dp[n-1][j1][j2] = grid[n-1][j1];
+                }else{
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+                }
             }
         }
-        return recMemo(0,0,c-1,r,c,grid,dp);
+        for(int i = n -2;i >= 0; i--){
+            for(int j1 =0; j1 < m; j1++){
+                for(int j2 = 0; j2 < m; j2++){
+//                     Copy paste
+                    int maxi = Integer.MIN_VALUE;
+                    for(int dj1 = -1; dj1<= 1; dj1++){
+                        for(int dj2 = -1; dj2 <= 1; dj2++){
+                            int value = 0;
+                            if(j1 == j2) value  = grid[i][j1];
+                            else value = grid[i][j1] + grid[i][j2];
+                            if(j1 + dj1 >=0 && j1 + dj1< m && j2+ dj2>=0 && j2+dj2 < m)
+                                value += dp[i+1][j1+dj1][j2+dj2];
+                            else 
+                                value = (int) Math.pow(-10,9);
+                            maxi = Math.max(maxi,value);
+                        }
+                    }
+                     dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 } 
