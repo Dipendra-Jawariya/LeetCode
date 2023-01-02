6ee755912e2f5bb9416ca3d375df0e176a13809b
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-01-02 10:11:53 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (97.51%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int n = word.length();
+        
+        boolean match1 = true,match2 = true, match3 = true;
+        
+        //case 1 : All Capital
+        for(int i =0;i<n;i++){
+            if(!Character.isUpperCase(word.charAt(i))){
+                match1 = false;
+                break;
+            }
+        }
+        if(match1) return true;
+        
+        //case 2: All Not Capital
+        for(int i=0;i<n;i++){
+            if(!Character.isLowerCase(word.charAt(i))){
+                match2 = false;
+                break;
+            }
+        }
+        if(match2) return true;
+        
+        //case 3 : All are not Capital
+        if(!Character.isUpperCase(word.charAt(0))){
+             match3 = false;
+        }
+        if(match3){
+            for(int i =1;i<n ;i++){
+                if(!Character.isLowerCase(word.charAt(i))){
+                    match3 = false;
+                    break;
+                }
+            }
+        }
+        if(match3) return true;
+        
+        //if not matching
+        return false;
+    }
+}
