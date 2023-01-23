42b6041eb474babffb9f88f4735c62579fd6a81c
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-01-23 20:04:49 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -31,46 +31,43 @@ class GFG {
 }
 // } Driver Code Ends
 
-class Pair {
-    int first ;
+class Pair{
+    int first;
     int second;
-    Pair(int first ,int second){
+    Pair(int first, int second){
         this.first = first;
         this.second = second;
     }
 }
 class Solution {
-    private boolean checkCycle(int src ,int V,ArrayList<ArrayList<Integer>> adj,
-    boolean vis[]){
+    // Function to detect cycle in an undirected graph.
+    private boolean f(int src,int V, ArrayList<ArrayList<Integer>> adj,boolean vis[]){
         vis[src] = true;
         Queue<Pair> q = new LinkedList<Pair>();
         q.add(new Pair(src,-1));
         while(!q.isEmpty()){
-            int node  = q.peek().first;
+            int node = q.peek().first;
             int parent = q.peek().second;
             q.remove();
             
-            //traverse the adj list
-            for(int adjacentNode : adj.get(node)){
-                if(!vis[adjacentNode]){
-                    vis[adjacentNode]  = true;
+            for(Integer adjacentNode : adj.get(node)){
+                if(vis[adjacentNode]!= true){
+                    vis[adjacentNode] = true;
                     q.add(new Pair(adjacentNode,node));
-                }else if(parent != adjacentNode){
+                }else if(parent!=adjacentNode){
                     return true;
-                     
                 }
             }
         }
         return false;
     }
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean vis[] = new boolean[V];
-        for(int i =0;i<V;i++){
-            vis[i] = false;
-        }
-        for(int i =0;i<V;i++){
-            if(!vis[i]){
-                if(checkCycle(i,V,adj,vis) == true) return true;
+        boolean vis[] = new boolean[V+1];
+        Arrays.fill(vis,false);
+        
+        for(int i=0;i<V;i++){
+            if(vis[i]==false){
+                if(f(i,V,adj,vis) == true) return true;
             }
         }
         return false;
