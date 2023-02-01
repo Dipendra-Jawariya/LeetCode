b9103b9880def59bd66af7c77708699689cd29f0
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-02 03:25:32 +0530

    Added solution - LeetHub

---
+++ Permutations in array - GFG/permutations-in-array.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(stt.nextToken());
+            long k = Long.parseLong(stt.nextToken());
+            long a[] = new long[n];
+            long b[] = new long[n];
+
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                b[i] = Long.parseLong(inputLine1[i]);
+            }
+
+            Solution obj = new Solution();
+            System.out.println(obj.isPossible(a, b, n, k) ? 1 : 0);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public boolean isPossible(long a[], long b[], int n, long k) {
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i = 0;i < a.length;i++){
+            if(a[i] + b[n-i-1] < k) return false;
+        }
+        return true;
+    }
+}
