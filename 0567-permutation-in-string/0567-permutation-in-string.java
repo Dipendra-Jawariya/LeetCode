c3982a8fe50595eb123f44b407f41b6fb27a98d2
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-04 23:34:45 +0530

    Time: 165 ms (22.58%), Space: 42.8 MB (41.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -29,23 +29,39 @@ class Solution {
     //         }
     // }
     
-    private String sort(String s){
-        char[] ch = s.toCharArray();
-        Arrays.sort(ch);
-        return String.valueOf(ch);
-    }
+//     private String sort(String s){
+//         char[] ch = s.toCharArray();
+//         Arrays.sort(ch);
+//         return String.valueOf(ch);
+//     }
+    
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        if(n > m ) return false;
-        s1 = sort(s1);
+        if(s1.length() > s2.length()){
+            return false;
+        }
+        int[] s1map = new int[26];
+        for(int i  = 0; i < s1.length(); i++){
+            s1map[s1.charAt(i) - 'a']++;
+        }
         
-        for(int i = 0; i <= m - n; i++){
-            String str = sort(s2.substring(i , i + n));
-            if(s1.equals(str)){
-                return true;
+        for(int i = 0; i <= s2.length() - s1.length(); i++){
+            int[] s2map = new int[26];
+            for(int j = 0; j < s1.length(); j++){
+                s2map[s2.charAt(i+j) - 'a']++;
+            }
+            if(matches(s1map,s2map)){
+            return true;
             }
         }
+        
         return false;
+    }
+    private boolean matches(int[] s1map,int[] s2map){
+        for(int i = 0; i < 26; i++){
+            if(s1map[i] != s2map[i]){
+                 return false;
+            }
+        }
+        return true;
     }
 }
