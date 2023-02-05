4034065876b02b7c9d8447978244c9bca99f4d54
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-05 20:02:33 +0530

    Time: 1 ms (87.77%), Space: 42.2 MB (67.79%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -19,30 +19,56 @@ class Solution {
         return dp[ind][buy] = profit;
     }
     public int maxProfit(int[] prices) {
+        //Tabulation code
+//         int n = prices.length;
+//         int dp[][] = new int[n+1][2];
+//         // for(int rows[] : dp){
+//         //     Arrays.fill(rows,0);
+//         // }
+        
+//         //base case
+//         dp[n][0] = dp[n][1] = 0;
+        
+//         for(int ind = n-1; ind >= 0; ind--){
+//             for(int buy = 0; buy <= 1; buy++){
+//                 int profit = Integer.MIN_VALUE;
+//                 if(buy == 1){ //buy --> buy 
+//                                 //  --> NOT BUY
+//                     profit = Math.max(-prices[ind] + dp[ind+1][0],
+//                                         0 + dp[ind+1][1]);
+//                 }else{ // sell -->  sell
+//                         //     --> not sell
+//                     profit = Math.max(prices[ind] + dp[ind+1][1],
+//                                       0 + dp[ind+1][0]);
+//                 }
+//                 dp[ind][buy] = profit;
+//             }
+//         }
+//         return dp[0][1];
+        
+        
+        // Space Optimization
         int n = prices.length;
-        int dp[][] = new int[n+1][2];
-        for(int rows[] : dp){
-            Arrays.fill(rows,0);
-        }
-        
-        //base case
-        dp[n][0] = dp[n][1] = 0;
-        
+        int ahead[] = new int[2];
+        int cur[] = new int[2];
+        Arrays.fill(ahead,0);
+        Arrays.fill(cur,0);
         for(int ind = n-1; ind >= 0; ind--){
             for(int buy = 0; buy <= 1; buy++){
                 int profit = Integer.MIN_VALUE;
                 if(buy == 1){ //buy --> buy 
                                 //  --> NOT BUY
-                    profit = Math.max(-prices[ind] + dp[ind+1][0],
-                                        0 + dp[ind+1][1]);
+                    profit = Math.max(-prices[ind] + ahead[0],
+                                        0 + ahead[1]);
                 }else{ // sell -->  sell
                         //     --> not sell
-                    profit = Math.max(prices[ind] + dp[ind+1][1],
-                                      0 + dp[ind+1][0]);
+                    profit = Math.max(prices[ind] + ahead[1],
+                                      0 + ahead[0]);
                 }
-                dp[ind][buy] = profit;
+                cur[buy] = profit;
             }
+            ahead = cur;
         }
-        return dp[0][1];
+        return ahead[1];
     }
 }
