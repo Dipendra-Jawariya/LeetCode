03af3c7147e4acf3c23d73f294b6772758947f26
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-06 06:31:01 +0530

    Added solution - LeetHub

---
+++ Searching in an array where adjacent differ by at most k - GFG/searching-in-an-array-where-adjacent-differ-by-at-most-k.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    int X = Integer.parseInt(br.readLine());
+		    
+		    Complete obj = new Complete();
+		    int ans = obj.search(arr, sizeOfArray, X, K);
+		    System.out.println(ans);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int search (int arr[], int n, int x, int k) {
+        if(arr[0] == x) return 0;
+        for(int i = 1; i < n; i++){
+            int diff = Math.abs(arr[i-1] - arr[i]);
+            if(diff <= k && arr[i] == x){
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    
+}
+
+
+
+
+
