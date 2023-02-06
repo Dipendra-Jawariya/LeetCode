646a5ab1d8386b98665216f4bdbf1f543ff4a24c
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-06 10:03:06 +0530

    Added solution - LeetHub

--- Alien Dictionary - GFG/alien-dictionary.java
+++ Alien Dictionary - GFG/alien-dictionary.java
@@ -85,23 +85,23 @@ class GFG {
 class Solution
 {
     private List<Integer> topoSort(int V,List<List<Integer>> adj){
+        List<Integer> ls = new ArrayList<>();
         int indegree[] = new int[V];
-        for(int i =0;i<V;i++){
-            for(int it:adj.get(i)){
+        for(int i = 0; i < V; i++){
+            for(int it : adj.get(i)){
                 indegree[it]++;
-            }
+            }    
         }
-        Queue<Integer> q= new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         for(int i =0;i<V;i++){
-            if(indegree[i] == 0) {
+            if(indegree[i] == 0){
                 q.add(i);
             }
         }
-        List<Integer> topo = new ArrayList<>();
         while(!q.isEmpty()){
             int node = q.peek();
             q.remove();
-            topo.add(node);
+            ls.add(node);
             for(int it : adj.get(node)){
                 indegree[it]--;
                 if(indegree[it] == 0){
@@ -109,32 +109,31 @@ class Solution
                 }
             }
         }
-        return topo;
+        return ls;
     }
     public String findOrder(String [] dict, int N, int K)
     {
-        // int[] adj = new int[K];
-        List<List<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>(K);
         for(int i =0;i<K;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i = 0; i < N -1;i++){
+        
+        for(int i = 0;i<N-1;i++){
             String s1 = dict[i];
             String s2 = dict[i+1];
             int len = Math.min(s1.length(),s2.length());
-            for(int ptr = 0;ptr < len;ptr++){
+            for(int ptr = 0; ptr < len; ptr++){
                 if(s1.charAt(ptr) != s2.charAt(ptr)){
-                    adj.get(s1.charAt(ptr) - 'a').add(s2.charAt(ptr)-'a');
+                    adj.get(s1.charAt(ptr) - 'a').add(s2.charAt(ptr) - 'a');
                     break;
-                }
+                }    
             }
         }
         List<Integer> topo = topoSort(K,adj);
-        String ans ="";
+        String ans = "";
         for(int it:topo){
-            ans = ans + (char)(it + (int)('a') );
+            ans  = ans + (char)(it+(int)('a'));
         }
         return ans;
-        
     }
 }
