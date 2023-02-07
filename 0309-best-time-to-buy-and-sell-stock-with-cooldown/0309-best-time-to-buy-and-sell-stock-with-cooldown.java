13a9ebca64250667ed883624a1f176029f76f85a
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-08 00:47:20 +0530

    Time: 1 ms (74.09%), Space: 40.6 MB (61.97%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -18,14 +18,9 @@ class Solution {
         for(int row[] : dp) Arrays.fill(row,0);
         
         for(int ind = n-1; ind >= 0; ind--){
-            for(int buy = 0; buy <= 1; buy++){
-                if(buy == 1){
-                     dp[ind][buy] = Math.max(-prices[ind] + dp[ind + 1][0],0+ dp[ind+1][1]);
-                }else{
-                    dp[ind][buy] = Math.max(prices[ind] + dp[ind+2][1],
-                                       0 + dp[ind+1][0]);
-                }
-            }
+             dp[ind][1] = Math.max(-prices[ind] + dp[ind + 1][0],0+ dp[ind+1][1]);
+             dp[ind][0] = Math.max(prices[ind] + dp[ind+2][1],0 + dp[ind+1][0]);
+            
         }
         return dp[0][1];
     }
