7b46b93390d796ace799875074ad332b0ea99113
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-08 00:40:03 +0530

    Time: 1 ms (74.09%), Space: 40.3 MB (81.40%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,21 +1,21 @@
 class Solution {
-    
-    private int func(int ind,int buy,int prices[],int dp[][]){
-        if(ind >= prices.length) return 0;
-        if(dp[ind][buy]!= -1) return dp[ind][buy];
-        if(buy == 1){
-            return dp[ind][buy] = Math.max(-prices[ind] + func(ind+1,0,prices,dp) ,
-                            0 +  func(ind+1,1,prices,dp));
+    private int f(int ind,int buy,int[] prices,int dp[][]){
+        if(ind >= prices.length) return  0;
+        if(dp[ind][buy] != -1){
+            return dp[ind][buy];           
         }
-            return dp[ind][buy] = Math.max(prices[ind] + func(ind+2,1,prices,dp),
-                            0 + func(ind+1,0,prices,dp));
-        
+        if(buy == 1){
+            return dp[ind][buy] = Math.max(-prices[ind] + f(ind + 1,0,prices,dp),
+                              0 + f(ind+1,1,prices,dp));
+        }
+        //for the cooldown we cannot go to the next day for buy just after selling the stock so we go to day after the day
+        return dp[ind][buy] = Math.max(prices[ind] + f(ind+2,1,prices,dp),
+                                       0 + f(ind+1,0,prices,dp));
     }
     public int maxProfit(int[] prices) {
-        int dp[][] = new int[prices.length][2];
-          for(int row[]: dp)
-            Arrays.fill(row,-1);
-    
-        return func(0,1,prices,dp);
+        int n  = prices.length;
+        int dp[][]= new int[n][2];
+        for(int row[] : dp) Arrays.fill(row,-1);
+        return f(0,1,prices,dp);
     }
 }
