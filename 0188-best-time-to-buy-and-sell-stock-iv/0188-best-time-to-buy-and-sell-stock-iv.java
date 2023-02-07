e4bdb41dccd12bdb8afc9b61d3c9919996813a34
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-08 00:02:35 +0530

    Time: 8 ms (46.91%), Space: 41.9 MB (69.14%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,41 +1,19 @@
 class Solution {
-    public int maxProfit(int k, int[] prices) {
-        int n = prices.length;
-        // int dp[][][] = new int[n+1][k+1][3];
-        int after[][] = new int[2][k+1];
-        int cur[][] = new int[2][k+1];
-        
-        for(int row[] : after) 
-            // for(int it[] : row){
-                Arrays.fill(row,0);
-            // }
-        
-        //base case
-        // for(int ind = 0;ind < n; ind++){
-        //     for(int buy = 0;buy < 2;buy++){
-        //         dp[ind][buy][cap] = 0;
-        //     }
-        // }
-        // //second base case
-        // for(int buy = 0;buy < 2;buy++){
-        //     for(int cap = 0;cap<2;cap++){
-        //         dp[n][buy][cap] = 0;
-        //     }
-        // }
-        
-        for(int ind = n-1;ind >= 0;ind--) {
-            for(int buy = 0;buy <= 1;buy++){
-                for(int cap = 1; cap <= k; cap++){
-                    if(buy == 1){
-                         cur[buy][cap] = Math.max(-prices[ind] + after[0][cap] ,
-                                         0  + after[1][cap]);
-                    }else{
-                         cur[buy][cap] = Math.max( prices[ind] + after[1][cap-1], 0 + after[0][cap]);
-                    }
-                }
-            }
-            after = cur;
+    private int f(int ind,int transNo,int prices[],int n ,int k,int dp[][]){
+        if(ind == n || transNo == 2 * k) return 0;
+        if(dp[ind][transNo] != -1) return dp[ind][transNo];
+        if(transNo % 2 == 0) //buy
+        {
+            return dp[ind][transNo] = Math.max(-prices[ind] + f(ind + 1,transNo + 1,prices,n,k,dp),
+                            0 + f(ind +1,transNo,prices,n,k,dp));
         }
-        return after[1][k];
+        return dp[ind][transNo] = Math.max(prices[ind] + f(ind + 1,transNo + 1,prices,n,k,dp),
+                        0 + f(ind + 1,transNo,prices,n,k,dp));
+    }
+    public int maxProfit(int k, int[] prices) {
+        int  n = prices.length;
+        int dp[][] = new int[n][2 * k]; 
+        for(int row[] : dp) Arrays.fill(row,-1);
+        return f(0,0,prices,n,k,dp);
     }
 }
