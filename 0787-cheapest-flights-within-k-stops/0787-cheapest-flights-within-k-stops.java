1249c34abf7668fd53319db35406b71f635766cd
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-21 03:01:27 +0530

    Time: 4 ms (97.21%), Space: 43.2 MB (76.43%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,62 +1,57 @@
-class Pair{
-    int first;
-    int second;
-    public Pair(int first,int second){
-        this.first = first;
-        this.second = second;
+class Tuple{
+    int stops;
+    int node;
+    int dist;
+    Tuple(int stops,int node,int dist) {
+        this.stops = stops;
+        this.node = node;
+        this.dist = dist;
     }
 }
-class Tuple {
-    int first, second, third; 
-    Tuple(int first, int second, int third) {
-        this.first = first; 
+class Pair {
+    int first;
+    int second;
+    Pair(int first,int second) {
+        this.first = first;
         this.second = second;
-        this.third = third; 
     }
 }
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i =0;i < n;i++){
+        for(int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        int m  = flights.length;
-        for(int i =0;i<m;i++){
+        int m = flights.length;
+        for(int i = 0; i < m; i++) {
             adj.get(flights[i][0]).add(new Pair(flights[i][1],flights[i][2]));
         }
         
-        Queue<Tuple> q=  new LinkedList<>();
-        // {stops,node,distance}
+        Queue<Tuple> q = new LinkedList<>();
         q.add(new Tuple(0,src,0));
         int dist[] = new int[n];
-        for(int i = 0;i<n;i++){
-            dist[i] = (int)(1e9);
-        }
+        Arrays.fill(dist,(int)(1e9));
         dist[src] = 0;
-         while(!q.isEmpty()) {
-            Tuple it = q.peek(); 
-            q.remove(); 
-            int stops = it.first; 
-            int node = it.second; 
-            int cost = it.third; 
+        while(!q.isEmpty()) {
+            Tuple it = q.peek();
+            q.remove();
+            int stops = it.stops;
+            int node = it.node;
+            int cost = it.dist;
             
-            // We stop the process as soon as the limit for the stops reaches.
-            if(stops > k) continue; 
-            for(Pair iter: adj.get(node)) {
-                int adjNode = iter.first; 
-                int edW = iter.second; 
+            if(stops > k) continue;
+            
+            for(Pair iter : adj.get(node)) {
+                int adjNode = iter.first;
+                int edW = iter.second;
                 
-                // We only update the queue if the new calculated dist is
-                // less than the prev and the stops are also within limits.
-                if (cost + edW < dist[adjNode] && stops <= k) {
-                    dist[adjNode] = cost + edW; 
-                    q.add(new Tuple(stops + 1, adjNode, cost + edW)); 
+                if(cost + edW < dist[adjNode] && stops <= k) {
+                    dist[adjNode] = cost + edW;
+                    q.add(new Tuple(stops + 1,adjNode,cost+edW));
                 }
             }
         }
-        // If the destination node is unreachable return ‘-1’
-        // else return the calculated dist from src to dst.
-        if(dist[dst] == (int)(1e9)) return -1; 
-        return dist[dst]; 
+        if(dist[dst] == (int)(1e9)) return -1;
+        return dist[dst];
     }
 }
