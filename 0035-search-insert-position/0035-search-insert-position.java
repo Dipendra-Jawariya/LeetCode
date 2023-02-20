06acaddc29fcc30f90587bd496571a55e6c9b93b
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-02-21 01:38:35 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (61.35%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,10 +4,10 @@ class Solution {
         int end = nums.length - 1;
         
         while(start <= end) {
-            int mid = start + (end - start) / 2;
+            int mid = (end + start) / 2;
             if(nums[mid] == target)  return mid;
-            else if(nums[mid] < target) start = mid +1;
-            else end = mid -1;
+            else if(nums[mid] < target) start = mid + 1;
+            else end = mid - 1;
         }
         return start;
     }
