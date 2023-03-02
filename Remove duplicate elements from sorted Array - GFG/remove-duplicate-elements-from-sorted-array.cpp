f0d9eee3e439ec7b85d19829cecb44e3893739d3
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-03-02 17:05:56 +0530

    Added solution - LeetHub

--- Remove duplicate elements from sorted Array - GFG/remove-duplicate-elements-from-sorted-array.cpp
+++ Remove duplicate elements from sorted Array - GFG/remove-duplicate-elements-from-sorted-array.cpp
@@ -11,16 +11,14 @@ using namespace std;
 class Solution{
 public:
     int remove_duplicate(int arr[],int n){
-        set<int> st;
-        for(int i = 0; i < n; i++) {
-            st.insert(arr[i]);
+        int i = 0;
+        for(int j =1; j <n;j++) {
+            if(arr[i] != arr[j]) {
+                arr[i+1] = arr[j];
+                i++;
+            }
         }
-        int index = 0;
-        for(auto it : st ) {
-            arr[index]  = it;
-            index++;
-        }
-        return index;
+        return i+1;
     }
 };
 
