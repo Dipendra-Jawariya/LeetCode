98faf0a317941d76def2c08258260882df7310f6
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-03-09 11:18:37 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (26.43%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,33 +10,23 @@
  * }
  */
 public class Solution {
-    private ListNode hasCycle(ListNode head) {
+    public ListNode detectCycle(ListNode head) {
         if(head == null) return null;
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next!= null) {
+        ListNode entry = head;
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast) {
-                return slow;
+            if(fast == slow) {
+                while(slow != entry) {
+                    entry = entry.next;
+                    slow = slow.next;
+                    
+                }
+                return entry;
             }
         }
         return null;
-    }
-    public ListNode detectCycle(ListNode head) {
-        if(head == null || head.next == null) {
-            return null;
-        }
-        
-        ListNode colloidal_point = hasCycle(head);
-        if(colloidal_point == null) {
-            return null;
-        }
-        ListNode entry = head;
-        while(entry != colloidal_point) {
-            entry = entry.next;
-            colloidal_point = colloidal_point.next;
-        }
-        return entry;
     }
 }
