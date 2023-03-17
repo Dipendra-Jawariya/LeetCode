4829cb5b105d480aab61a9b2ce95e5788aec2f0a
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-03-17 19:53:50 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int climbStairs(int nStairs) {
-        vector<int> dp(nStairs+1,-1);
-        dp[0] = 1;
-        dp[1] = 1; 
+//         vector<int> dp(nStairs+1,-1);
+//         dp[0] = 1;
+//         dp[1] = 1; 
+//         for(int i = 2; i <= nStairs; i++) {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[nStairs];
+        
+        //space optimization
+        int prev1 = 1;
+        int prev2 = 1;
         for(int i = 2; i <= nStairs; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[nStairs];
+        return prev1;
     }
 };
