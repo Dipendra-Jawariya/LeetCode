489cb1d47a60ad8590fb55c85e6f504768c64179
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-03-21 16:35:28 +0530

    Time: 14 ms (94.18%), Space: 19.7 MB (38.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> mpp;
-        for(int i= 0 ;i < nums.size(); i++) {
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp) {
-            if(it.second > nums.size() / 2) {
-                return it.first;
+         //MOORE'S VOTING ALGORITHM
+        int n = nums.size();
+        int cnt = 0;
+        int el;
+        //apply moore's voting algoritm
+        for(int i = 0; i < n; i++) {
+            if(cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            } else if(el == nums[i]) {
+                cnt++;
+            } else {
+                cnt--;
             }
         }
+        int cnt1 = 0;
+        //verify if there is any element which is greater n/2
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == el) {
+                cnt1++;
+            }
+        }
+        
+        if(cnt1 > n/2) return el;
         return -1;
     }
 };
