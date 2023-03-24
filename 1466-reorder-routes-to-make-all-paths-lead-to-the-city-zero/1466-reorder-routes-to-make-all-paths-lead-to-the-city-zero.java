d416ab0c62a012b158d55da5ab4c15432840c521
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-03-24 19:05:19 +0530

    Time: 575 ms (5.02%), Space: 72.8 MB (52.27%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,38 @@
+class Pair {
+    int node;
+    int edge;
+    Pair(int node,int edge){
+        this.node =  node;
+        this.edge = edge;
+    }
+}
+class Solution {
+    private int dfs(int node,int parent,ArrayList<ArrayList<Pair>> adj,int[] vis) {
+        int cnt = 0;
+        vis[node] = 1;
+        for(Pair list : adj.get(node)) {
+            if(vis[list.node] == -1) {
+                System.out.println("Node:" + node +" "+ list.node + " " + list.edge);
+                // if(list.node == parent) continue;
+                cnt += list.edge + dfs(list.node,node,adj,vis);    
+            }
+        }
+        return cnt;
+    }
+    public int minReorder(int n, int[][] connections) {
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for(int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        int m = connections.length;
+        for(int i = 0; i < m; i++) {
+            int u = connections[i][0];
+            int v = connections[i][1];
+            adj.get(u).add(new Pair(v,1)); // original edge
+            adj.get(v).add(new Pair(u,0)); // artificial edge
+        }
+        int vis[] = new int[n];
+        Arrays.fill(vis,-1);
+        return dfs(0,-1,adj,vis);
+    }
+}
