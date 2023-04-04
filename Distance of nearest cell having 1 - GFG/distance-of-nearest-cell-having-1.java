bad4407a1d1674e46b5df9f64ebbd2e0c1b42bb9
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-04-04 20:44:10 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.java
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.java
@@ -36,11 +36,11 @@ class GFG
 class Node{
     int row;
     int col;
-    int dis;
-    Node(int row,int col,int dis){
+    int dist;
+    Node(int row,int col,int dist){
         this.row = row;
         this.col = col;
-        this.dis = dis;
+        this.dist = dist;
     }
 }
 class Solution
@@ -48,56 +48,39 @@ class Solution
     //Function to find distance of nearest 1 in the grid for each cell.
     public int[][] nearest(int[][] grid)
     {
-        int n = grid.length; 
-	    int m = grid[0].length; 
-	    // visited and distance matrix
-	    int vis[][] = new int[n][m]; 
-	    int dist[][] = new int[n][m]; 
-	    // <coordinates, steps>
-	    Queue<Node> q = new LinkedList<Node>();
-	    // traverse the matrix
-	    for(int i = 0;i<n;i++) {
-	        for(int j = 0;j<m;j++) {
-	        // start BFS if cell contains 1
-	            if(grid[i][j] == 1) {
-	                q.add(new Node(i, j, 0)); 
-	                vis[i][j] = 1; 
-	            }
-	            else {
-	                // mark unvisted 
-	                vis[i][j] = 0; 
-	            }
-	        }
-	    }
+        int n = grid.length;
+        int m = grid[0].length;
+        int vis[][] = new int[n][m];
+        int dist[][] = new int[n][m];
+        Queue<Node> q = new LinkedList<>();
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < m; col++) {
+                if(grid[row][col] == 1) {
+                    vis[row][col] = 1;
+                    q.add(new Node(row,col,0));
+                } else {
+                    vis[row][col] = 0;
+                }
+            }
+        }
+        int delRow[] = {-1, 0, +1, 0}; 
+	    int delCol[] = {0, +1, 0, -1};
 	    
-	    
-	    
-	    int delrow[] = {-1, 0, +1, 0}; 
-	    int delcol[] = {0, +1, 0, -1}; 
-	    
-	    
-	    // n x m x 4 
-	    // traverse till queue becomes empty
-	    while(!q.isEmpty()) {
-	        int row = q.peek().row; 
-	        int col = q.peek().col; 
-	        int steps = q.peek().dis; 
-	        q.remove(); 
-	        dist[row][col] = steps; 
-	        // for all 4 neighbours
-	        for(int i = 0;i<4;i++) {
-	            int nrow = row + delrow[i]; 
-	            int ncol = col + delcol[i]; 
-	            // check for valid unvisited cell
-	            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-	            && vis[nrow][ncol] == 0)  {
-	                    vis[nrow][ncol] = 1; 
-    	            q.add(new Node(nrow, ncol, steps+1));  
-	            } 
-	            }
-	        }
-	    
-	    // return distance matrix
-	    return dist; 
+        while(!q.isEmpty()) {
+           int row = q.peek().row;
+           int col = q.peek().col;
+           int steps = q.peek().dist;
+           q.remove();
+           dist[row][col] = steps;
+           for(int i = 0; i < 4; i++) {
+               int nrow = row + delRow[i];
+               int ncol = col + delCol[i];
+               if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0) {
+                   vis[nrow][ncol] = 1;
+                   q.add(new Node(nrow,ncol,steps+1));
+               }
+           }
+        }
+        return dist;
     }
 }
