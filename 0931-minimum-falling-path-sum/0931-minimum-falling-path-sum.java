d3baa390e1c621b63b041ef474c75915d8b347ee
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-04-07 20:44:23 +0530

    Time: 7 ms (14.98%), Space: 43.1 MB (55.47%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -16,11 +16,31 @@ class Solution {
         int maxi = Integer.MAX_VALUE;
         int dp[][] = new int[n][m];
         for(int row[]:dp){
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
         }
         for(int j = 0; j < m; j++){
-            int ans = getMaxSum(n-1,j,m,matrix,dp);
-            maxi = Math.min(maxi,ans);
+            dp[0][j] = matrix[0][j];
+            // int ans = getMaxSum(n-1,j,m,matrix,dp);
+            // maxi = Math.min(maxi,ans);
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int up = matrix[i][j] + dp[i-1][j];
+                int leftDia = (int) 1e9;
+                if(j-1 >= 0) {
+                   leftDia =  matrix[i][j] + dp[i-1][j-1];   
+                }
+                
+                int rightDia = (int) 1e9;
+                if(j+1 < m) {
+                   rightDia = matrix[i][j] + dp[i-1][j+1];   
+                }
+                dp[i][j] = Math.min(up,Math.min(leftDia,rightDia));
+            }
+        }
+        maxi = dp[n-1][0];
+        for(int j = 1; j < m; j++) {
+            maxi = Math.min(dp[n-1][j],maxi);
         }
         return maxi;
     }
