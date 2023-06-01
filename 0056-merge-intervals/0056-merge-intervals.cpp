d4a383848b4e134b62d37cf9fd07851808bce873
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-06-01 19:23:06 +0530

    Time: 42 ms (59.87%), Space: 18.9 MB (81.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,20 +6,11 @@ public:
         vector<vector<int>> ans;
         
         for(int i = 0 ; i < n; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end <= ans.back()[1]) {
-                continue;
+            if(ans.empty() || intervals[i][0] > ans.back()[1]) {
+                ans.push_back(intervals[i]);
             } else {
-                for(int j = i+1; j < n; j++) {
-                    if(intervals[j][0] <= end) {
-                        end = max(end,intervals[j][1]);
-                    } else {
-                        break;
-                    }
-                }
+                ans.back()[1]  = max(ans.back()[1] , intervals[i][1]);
             }
-            ans.push_back({start,end});
         }
         return ans;
     }
