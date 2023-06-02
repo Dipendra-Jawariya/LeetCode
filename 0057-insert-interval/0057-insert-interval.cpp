ee321ceab33413a2d4b8fea33191c47b8858f18f
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-06-02 11:50:26 +0530

    Time: 19 ms (55.59%), Space: 17.1 MB (62.98%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end());
-        
-        for(auto it : intervals) {
-            for(auto iter : it) {
-                cout<< iter<< " ";
-            } cout<<endl;
-        }
-        
         int n = intervals.size();
         vector<vector<int>> ans;
+        
         for(int i = 0; i < n; i++) {
-            if(ans.empty() || intervals[i][0] > ans.back()[1]) {
+            if(intervals[i][0] > newInterval[1]) {
+                ans.push_back(newInterval);
+                for(int j = i;j < n; j++) {
+                    ans.push_back(intervals[j]);
+                }
+                return ans;
+            }
+            else if(newInterval[0] > intervals[i][1]) {
                 ans.push_back(intervals[i]);
             } else {
-                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+                newInterval[0] = min(intervals[i][0],newInterval[0]);
+                newInterval[1] = max(intervals[i][1],newInterval[1]);
             }
         }
+        ans.push_back(newInterval);
         return ans;
     }
 };
