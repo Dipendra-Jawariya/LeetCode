f91c0ff2f466ec67d766feaad02de36ff09b8562
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-07-03 07:50:33 +0530

    Time: 12 ms (46.53%), Space: 13.5 MB (73.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,45 @@
 class Solution {
 private:
-    int lowerBound(vector<int> &nums,int target,int n) {
-        int low = 0, high = n - 1;
-        int ans = n;
+    int first(vector<int> & nums,int target,int n) {
+        int low = 0,high = n - 1;
+        int ans = -1;
         while(low <= high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] >= target) {
+            if(nums[mid] == target) {
                 ans = mid;
                 high = mid - 1;
-            } else {
+            } else if(nums[mid] < target) {
                 low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return ans;
     }
+    
 private:
-    int upperBound(vector<int> &nums,int target,int n) {
+    int last(vector<int> &nums,int target,int n ) {
         int low = 0, high = n - 1;
-        int ans = n;
+        int ans = -1;
         while(low <= high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] > target) {
+            if(nums[mid] == target) {
                 ans = mid;
-                high = mid - 1;
-            } else {
                 low = mid + 1;
+            } else if(nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return ans;
     }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        // Plain Binary Search
         int n = nums.size();
-        int lb = lowerBound(nums,target,n);
-        if(lb == n || nums[lb] != target) return {-1,-1};
-        
-        int up = upperBound(nums,target,n);
-        return {lb, up - 1};
+        int frst = first(nums,target,n);
+        int lst = last(nums,target,n);
+        return {frst,lst};
     }
 };
