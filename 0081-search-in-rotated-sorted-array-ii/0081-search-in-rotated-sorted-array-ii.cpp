db0914424b81306794d761dafbe9f9a523ba9f86
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-07-04 06:59:46 +0530

    Time: 8 ms (39.88%), Space: 14.1 MB (19.56%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+         int n = nums.size();
+        int low = 0, high = n - 1;
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(nums[mid] == target) return true;
+            
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){ 
+                low ++;
+                high--; 
+                continue;
+            }
+            // left sorted search space
+            if(nums[low] <= nums[mid] ) {
+                if(nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // right sorted search space
+            else {
+                if(nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
