a01db3c95bce522b38b3d5ec5fcccf5afdffe4e0
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-07-20 23:22:28 +0530

    Time: 15 ms (71.71%), Space: 17.7 MB (76.60%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        for(int asteroid : asteroids) {
+            int flag = 1;
+            while(!st.empty() && (st.top() > 0 && asteroid < 0)) {
+                if(abs(st.top()) < abs(asteroid)) {
+                    st.pop();
+                    continue;
+                } else if(abs(st.top()) == abs(asteroid)) {
+                    st.pop();
+                }
+                
+                flag = 0;
+                break;
+            }
+            if(flag) {
+                st.push(asteroid);
+            }
+        }
+        vector<int> ans(st.size());
+        for(int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
