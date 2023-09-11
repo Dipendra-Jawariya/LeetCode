f725805c30cdd69258ca7d6186d1acdd332a286f
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-09-11 12:42:35 +0530

    Time: 140 ms (98.19%), Space: 114.2 MB (80.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -9,17 +9,51 @@
  * };
  */
 class Solution {
+private:
+    ListNode* findMiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast -> next && fast -> next -> next) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+            
+        }
+        return slow;
+    }
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while(curr != NULL) {
+            ListNode* next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-        while(head != NULL) {
-            arr.push_back(head->val);
-            head = head -> next;
-        }
-        for(int i = 0; i < arr.size() / 2; i++) {
-            if(arr[i] != arr[arr.size() - i - 1]) {
-                 return false;
+        
+        if(head == NULL && head -> next == NULL) {
+            return true;
+         }
+        
+        ListNode* middle = findMiddle(head);
+        ListNode* reverse = reverseList(middle -> next);
+        middle -> next = NULL;
+        
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = reverse;
+        
+        while(secondHalf != NULL) {
+            if(firstHalf -> val != secondHalf -> val) {
+                return false;
             }
+            
+            firstHalf = firstHalf -> next;
+            secondHalf = secondHalf -> next;
         }
         return true;
     }
