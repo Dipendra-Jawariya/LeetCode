842de45004a50e0ba7191712819ef01ffa3c9e18
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-09-12 14:49:03 +0530

    Time: 55 ms (62.90%), Space: 17.3 MB (77.52%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<char,int> mpp;
-        for(char ch : s) {
-            mpp[ch]++;
+        vector<int> freq(26,0);
+        for(char it : s) {
+            freq[it - 'a']++;
         }
-        int ans = 0;
-        set<int> st;
-        for(auto [ch,count] : mpp) {
-            while(st.find(count) != st.end()) {
-                count--;
-                ans++;
-            }
-            if(count > 0) {
-                st.insert(count);
+        sort(freq.begin(),freq.end());
+        int del_operation = 0;
+        for(int i = 24; i >= 0; i--) {
+            if(freq[i] == 0) break;
+            
+            if(freq[i] >= freq[i+1]) {
+                int prev = freq[i];
+                freq[i] = max(0,freq[i+1] - 1);
+                del_operation += prev - freq[i];
             }
         }
-        return ans;
+        return del_operation;
     }
 };
