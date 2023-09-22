3f68c18a1ed3b814d0965815e150766fa65c4770
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2023-09-22 20:06:43 +0530

    Time: 12 ms (17.81%), Space: 14.5 MB (15.85%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div733-flood-fill/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div733-flood-fill.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    void bfs(int sr,int sc,int initialColor,int color,vector<vector<int>>& image,
+             vector<vector<int>>& ans) {
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        
+        int delRow[] = {-1,0,1,0};
+        int delCol[] = {0,1,0,-1};
+        
+        int n = image.size();
+        int m = image[0].size();
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            ans[row][col] = color;
+            for(int i = 0; i < 4; i++) {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
+                   image[nrow][ncol] == initialColor && ans[nrow][ncol] != color) {
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int initialColor = image[sr][sc];
+        vector<vector<int>> ans = image;
+        bfs(sr,sc,initialColor,color,image,ans);
+        return ans;
+    }
+};
